1da4f610943a16edf00995af43c627e2d2d11c33
HibathRahman1 <hibathrahman.m@aimwindow.co.in>
2025-08-08 19:15:43 +0530

    successfully created

---
+++ script.js
@@ -0,0 +1,48 @@
+
+/* For Skills Skection */
+
+window.addEventListener('DOMContentLoaded', () => {
+    const progresses = document.querySelectorAll('.progress');
+    const percents = document.querySelectorAll('.percent');
+  
+    percents.forEach((el, index) => {
+      const percent = parseInt(el.dataset.percent);
+      const progress = progresses[index];
+      
+      let current = 0;
+      const interval = setInterval(() => {
+        if (current >= percent) {
+          clearInterval(interval);
+        } else {
+          current++;
+          el.textContent = `${current}%`;
+          progress.style.width = `${current}%`;
+        }
+      }, 20); 
+    });
+  });
+  
+
+
+
+
+
+
+/* For contact and submit */
+
+  document.getElementById("contactform").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const status = document.getElementById("formStatus");
+  
+    status.textContent = "Sending...";
+    setTimeout(() => {
+      status.textContent = "Message sent! ✅";
+      this.reset();
+    }, 1000);
+  });
+  
+
+
+
+
+ 
