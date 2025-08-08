16ac05170a48c5594bd3484874e71faf819442c8
riyaskhan17 <riyashalidh07@gmail.com>
2025-08-08 19:58:58 +0530

    successfully created

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-
 /* For Skills Skection */
 
-window.addEventListener('DOMContentLoaded', () => {
-    const progresses = document.querySelectorAll('.progress');
-    const percents = document.querySelectorAll('.percent');
-  
-    percents.forEach((el, index) => {
-      const percent = parseInt(el.dataset.percent);
-      const progress = progresses[index];
-      
-      let current = 0;
-      const interval = setInterval(() => {
-        if (current >= percent) {
-          clearInterval(interval);
-        } else {
-          current++;
-          el.textContent = `${current}%`;
-          progress.style.width = `${current}%`;
-        }
-      }, 20); 
-    });
+window.addEventListener("DOMContentLoaded", () => {
+  const progresses = document.querySelectorAll(".progress");
+  const percents = document.querySelectorAll(".percent");
+
+  percents.forEach((el, index) => {
+    const percent = parseInt(el.dataset.percent);
+    const progress = progresses[index];
+
+    let current = 0;
+    const interval = setInterval(() => {
+      if (current >= percent) {
+        clearInterval(interval);
+      } else {
+        current++;
+        el.textContent = `${current}%`;
+        progress.style.width = `${current}%`;
+      }
+    }, 20);
   });
-  //hiii
-
-
-
-
-
+});
+//hiii
 
 /* For contact and submit */
+document.getElementById("contactform").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const status = document.getElementById("formStatus");
 
-  document.getElementById("contactform").addEventListener("submit", function (e) {
-    e.preventDefault();
-    const status = document.getElementById("formStatus");
-  
-    status.textContent = "Sending...";
-    setTimeout(() => {
-      status.textContent = "Message sent! ✅";
-      this.reset();
-    }, 1000);
-  });
-  
-
-
-
-
- 
+  status.textContent = "Sending...";
+  setTimeout(() => {
+    status.textContent = "Message sent! ✅";
+    this.reset();
+  }, 1000);
+});
